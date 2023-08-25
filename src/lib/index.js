3fb5851b4f193dc1333384ad97d62b35a317247d
lurkydismal <lurkydismal@duck.com>
2023-08-25 10:38:48 -0700

    Remove non-kusa files from origin

--- src/lib/index.js
+++
@@ -1,4 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-export { default as ThemeToggle } from './components/ThemeToggle/ThemeToggle.svelte'
-export { default as Match } from './components/Match/Match.svelte'
-export { default as Pager } from './components/Pager/Pager.svelte'
--- src/lib/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type Record = {
-	 id: number,
-	 winner: string,
-	 loser: string,
-	 time: string
-}
